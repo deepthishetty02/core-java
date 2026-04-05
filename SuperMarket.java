b7fd57689b08ec8514f12eb9176d906279691197
deepthishetty02 <deepthideepu749@gmail.com>
2026-04-05 12:11:43 +0530

    delete

--- SuperMarket.java
+++
@@ -1,7 +0,0 @@
-class SuperMarket {
-
-    static String marketName = "Fresh Mart";
-    static int totalItems = 1200;
-    static double openingTime = 9.30;
-
-}
--- SwiggyRunner.java
+++
@@ -1,9 +0,0 @@
-class SwiggyRunner {
-
-    public static void main(String[] args) {
-
-        Swiggy order1 = new Swiggy();
-        
-
-    }
-}
